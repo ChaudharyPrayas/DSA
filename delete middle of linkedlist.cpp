22baa52ac8109e1656a16b24216b8dc1c296719a
Prayas Chaudhary <113814551+ChaudharyPrayas@users.noreply.github.com>
2023-03-05 14:08:17 +0530

    Add files via upload

---
+++ delete middle of linkedlist.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref,
+           int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+struct Node* deletemiddle(struct Node *head);
+
+void printList(Node *head)
+{
+   while (head != NULL)
+   {
+      cout << head->data << " ";
+      head = head->next;
+   }
+   cout << "\n";
+}
+ struct Node* deletemiddle(struct Node* head) {
+                struct Node *ptr;
+        int n=1;
+        ptr=head;
+        if(ptr==NULL||ptr->next==NULL)
+        return NULL;
+        while(ptr->next!=NULL)
+        {
+        ptr=ptr->next;
+n++;     
+   }
+   ptr=head;
+ 
+   for(int i=1;i<n/2;i++)
+   {
+    ptr=ptr->next;
+   }
+    ptr->next=ptr->next->next;
+   return head;
+    }
+    int main()
+{
+  int T, i, n, l;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+    head = deletemiddle(head);
+    printList(head);
+    }
+    return 0;
+}
