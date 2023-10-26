d8be6bfe568716c091b20b4435a61100b5542405
ChaudharyPrayas <itzzzprayas@gmail.com>
2023-10-26 14:19:19 +0530

    prefix sum

--- DSAChallenge/day39/prefix_sum.cpp
+++ DSAChallenge/day39/prefix_sum.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> arr;
+    int n,j,k;
+    cout<<"enter size of array"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int in;
+        cin>>in;
+        arr.push_back(in);
+    }
+    vector<int>prefix(n);
+    int pr=0;
+    for(int i=0;i<n;i++)
+    {
+           pr+=arr[i];
+           prefix[i]=pr;
+    }
+    cout<<"the array is:"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"the array with preix sum is:"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<prefix[i]<<" ";
+    }
+    
+}
 
