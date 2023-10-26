f1851c7b32c85a70d669a9077386eead4edfdc2b
ChaudharyPrayas <itzzzprayas@gmail.com>
2023-10-26 07:27:40 +0530

    subarrray

---
+++ DSAChallenge/day39/tempCodeRunnerFile.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> arr;
+    int n,j,k;
+    cout<<"enter size of array"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int in;
+        cin>>in;
+        arr.push_back(in);
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"subarray of size"<<i<<endl;
+        for(j=0;j<n;j++)
+        { if(j+i>n)
+                break;
+            cout<<j+i<<":-";
+       
+            for(k=j;k<j+i;k++)
+            {
+                
+             cout<<arr[k]<<" ";
+        }}
+        cout<<endl;
+    }
+}
