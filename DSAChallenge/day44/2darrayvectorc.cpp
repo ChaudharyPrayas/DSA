97b3c24cb603f27c3c5cf6c4292453d5a1966f58
ChaudharyPrayas <itzzzprayas@gmail.com>
2023-10-31 22:46:30 +0530

    abc

--- DSAChallenge/day44/2darrayvectorc.cpp
+++ DSAChallenge/day44/2darrayvectorc.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<vector>
+int main()
+{int row,col;
+cout<<"enter rows and colums respectiveLy";
+
+cin>>row;
+cin>>col;
+    vector<vector<int>> mat(row,vector<int>(col,0));
+    int i,j;
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"rows="<<mat.size()<<" "<<endl;
+     cout<<"colums="<<mat[0].size()<<" ";
+     //updation of valures
+     for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            cin>>mat[i][j];
+        }
+    }
+    //after updation
+     for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 
